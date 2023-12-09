02e44f02ce5e657d0b956f594ca59e56fad9d719
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2023-12-09 18:18:04 +0200

    fix: width for component skeleton

--- src/components/Comments/Comment.tsx
+++ src/components/Comments/Comment.tsx
@@ -20,7 +20,7 @@ export function CommentSkeleton() {
     <li className="flex items-start mb-4 animate-pulse">
       <div className="flex-shrink-0 mr-2 w-12 h-12 bg-gray-300 rounded-full"></div>
 
-      <div className="bg-white shadow-lg rounded-lg p-4">
+      <div className="bg-white shadow-lg rounded-lg p-4 w-full">
         <div className="w-full h-4 bg-gray-300 rounded"></div>
         <div className="mt-2 w-3/4 h-4 bg-gray-300 rounded"></div>
         <div className="mt-2 w-1/2 h-4 bg-gray-300 rounded"></div>
